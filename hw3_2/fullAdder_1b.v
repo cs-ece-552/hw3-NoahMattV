c3c9ce6b3f45930475aeb77e88b8a4580c8a3c25
Noah Van Der Weide <noahmattv@gmail.com>
2019-02-12 13:27:42 -0600

    update after pulling fixes from Prof

--- hw3_2/fullAdder_1b.v
+++ hw3_2/fullAdder_1b.v
@@ -10,16 +10,13 @@ module fullAdder_1b(A, B, C_in, S, C_out);
     output S;
     output C_out;
 
-<<<<<<< HEAD
   wire ab_xor, abc_nand, ab_nand;
-  
+
   xor2 xor_0 (.in1(A), .in2(B), .out(ab_xor));
   xor2 xor_1 (.in1(ab_xor), .in2(C_in), .out(S));
   nand2 nand_0 (.in1(ab_xor), .in2(C_in), .out(abc_nand));
   nand2 nand_1 (.in1(A), .in2(B), .out(ab_nand));
   nand2 nand_2 (.in1(abc_nand). in2(ab_nand), .out(C_out));
-=======
-    // YOUR CODE HERE
->>>>>>> hw3/master
+
 
 endmodule
--- hw3_2/rca_16b.v
+++ hw3_2/rca_16b.v
@@ -14,17 +14,11 @@ module rca_16b(A, B, C_in, S, C_out);
     output [N-1:0] S;
     output         C_out;
 
-<<<<<<< HEAD
-
   wire c_0, c_1, c_2, c_3;
 
   rca_4b rca_4b_0 (.A(A[3:0]), .B(B[3:0]), .C_in(c_0), .S(S[3:0]), .C_out(c_1));
   rca_4b rca_4b_1 (.A(A[7:4]), .B(B[7:4]), .C_in(c_1), .S(S[7:4]), .C_out(c_2));
   rca_4b rca_4b_2 (.A(A[11:8]), .B(B[11:8]), .C_in(c_2), .S(S[11:8]), .C_out(c_3));
   rca_4b rca_4b_3 (.A(A[15:12]), .B(B[15:12]), .C_in(c_3), .S(S[15:12]), .C_out(C_out));
-
-=======
-    // YOUR CODE HERE
->>>>>>> hw3/master
 
 endmodule
